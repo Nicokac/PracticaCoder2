92c06bf13920f34b3c62bbd05e55b1db6b714590
Nicokac <kachuk_nm@hotmail.com>
2024-09-22 19:42:02 -0300

    Se agregan formularios de busqueda de cursos y para crear cursos nuevos

--- app_coder/urls.py
+++ app_coder/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import crea_curso, lista_cursos, profesores, cursos, estudiantes, entregables, inicio
+from .views import crea_curso, lista_cursos, profesores, cursos, estudiantes, entregables, inicio, curso_formulario, busqueda_camada, buscar_camada
 
 urlpatterns = [
     path('agrega-curso/<nombre>/<camada>', crea_curso),
@@ -9,5 +9,8 @@ urlpatterns = [
     path('cursos/', cursos, name="Cursos"),
     path('estudiantes/', estudiantes, name="Estudiantes"),
     path('entregables/', entregables, name="Entregables"),
+    path('curso-formulario/', curso_formulario, name="CursoFormulario"),
+    path('busqueda-camada/', busqueda_camada, name="BusquedaCamada"),
+    path('buscar-camada/', buscar_camada, name="BuscarCamada"),
 
 ]
