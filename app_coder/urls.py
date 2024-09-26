0b5be01c77dc0daf2f1cde463175c61cdb3b5ea9
Nicokac <kachuk_nm@hotmail.com>
2024-09-25 23:00:00 -0300

    Se agrega validación para la creación de cursos, se permite agregar cursos y profesores, además de mostrar esos datos en una tabla.

--- app_coder/urls.py
+++ app_coder/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
-from .views import crea_curso, lista_cursos, profesores, cursos, estudiantes, entregables, inicio, curso_formulario, busqueda_camada, buscar_camada
+from .views import crea_curso, lista_cursos, profesores, cursos, estudiantes, entregables, inicio, curso_formulario, busqueda_camada, buscar_camada, profesorFormulario, lista_profesores
 
 urlpatterns = [
     path('agrega-curso/<nombre>/<camada>', crea_curso),
     path('lista-cursos/', lista_cursos),
     path("", inicio, name="Inicio"),
-    path('profesores/', profesores, name="Profesores"),
+    #path('profesores/', profesores, name="Profesores"),
+    path('profesores/', lista_profesores, name="Profesores"),
     path('cursos/', cursos, name="Cursos"),
     path('estudiantes/', estudiantes, name="Estudiantes"),
     path('entregables/', entregables, name="Entregables"),
     path('curso-formulario/', curso_formulario, name="CursoFormulario"),
     path('busqueda-camada/', busqueda_camada, name="BusquedaCamada"),
     path('buscar-camada/', buscar_camada, name="BuscarCamada"),
-
+    path('profesorFormulario/', profesorFormulario, name="ProfesorFormulario"),
 ]
--- app_coder/views.py
+++ app_coder/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-from .models import Curso
-from .forms import CursoFormulario
+from .models import Curso, Profesor
+from .forms import CursoFormulario, ProfesorFormulario
 
 # Create your views here.
 def crea_curso(req, nombre, camada):
@@ -15,8 +15,8 @@ def crea_curso(req, nombre, camada):
 def lista_cursos(req):
     
     lista = Curso.objects.all()
-
-    return render(req, "lista_cursos.html", {"lista_cursos": lista})
+    print("Cursos encontrados: ", lista)
+    return render(req, "cursos.html", {"lista_cursos": lista})
 
 def inicio(req):
 
@@ -56,10 +56,25 @@ def curso_formulario(req):
 
             data = mi_formulario.cleaned_data
 
+             # Validaciones básicas
+            errores = []
+
+            # Verificar si el nombre está vacío
+            if not data["curso"].strip():
+                errores.append("El nombre del curso no puede estar vacío.")
+            
+            # Verificar si la camada es un número positivo
+            if data["camada"] <= 0:
+                errores.append("La camada debe ser un número positivo.")
+
+            # Si hay errores, mostrar el formulario con los errores
+            if errores:
+                return render(req, "curso_formulario.html", {"mi_formulario": mi_formulario, "errores": errores})
+
             nuevo_curso = Curso(nombre=data["curso"], camada=data["camada"])
             nuevo_curso.save()
-            return render(req, "inicio.html", {})
-        
+            #return render(req, "inicio.html", {})
+            return render(req, "inicio.html", {"mensaje": f"Curso: {nuevo_curso.nombre} - Camada {nuevo_curso.camada} creado con éxito!"})
         else:
             return render(req, "curso_formulario.html", {"mi_formulario": mi_formulario})
         
@@ -79,3 +94,44 @@ def buscar_camada(req):
     cursos = Curso.objects.filter(camada__icontains=num_camada)
 
     return render(req, "resultado_busqueda.html", {"cursos": cursos, "camada": num_camada})
+
+def profesorFormulario(request):
+
+    if request.method == 'POST':
+
+        miformulario = ProfesorFormulario(request.POST)
+        print(miformulario)
+
+        if miformulario.is_valid():
+
+            data = miformulario.cleaned_data
+
+            #  # Validaciones básicas
+            # errores = []
+
+            # # Verificar si el nombre está vacío
+            # if not data["curso"].strip():
+            #     errores.append("El nombre del curso no puede estar vacío.")
+            
+            # # Verificar si la camada es un número positivo
+            # if data["camada"] <= 0:
+            #     errores.append("La camada debe ser un número positivo.")
+
+            # # Si hay errores, mostrar el formulario con los errores
+            # if errores:
+            #     return render(request, "curso_formulario.html", {"miformulario": miformulario, "errores": errores})
+
+            profesor = Profesor(nombre=data["nombre"], apellido=data["apellido"], email=data["email"], profesion=data["profesion"])
+            profesor.save()
+            return render(request, "inicio.html", {"mensaje": f"Nombre: {profesor.nombre} - Apellido: {profesor.apellido} creado con éxito!"})
+        else:
+            return render(request, "profesorFormulario.html", {"miformulario": miformulario})
+        
+    else:
+
+        miformulario = ProfesorFormulario()
+    return render(request, "profesorFormulario.html", {"miformulario": miformulario})
+
+def lista_profesores(request):
+    profesores = Profesor.objects.all()  # Obtener todos los profesores
+    return render(request, "profesores.html", {"profesores": profesores})
